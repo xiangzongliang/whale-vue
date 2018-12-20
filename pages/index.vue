42b536d87065e55990da129e3ecf45eb5450e904
向亮 <826463893@qq.com>
2018-12-20 23:44:48 +0800

    提交文件

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="example">{{ title }}
+      <img src="https://www.xiangzongliang.com/_nuxt/img/logoIco.35b4973.png"/>
       <input v-model="inputVal"/>
       <span @click="postMsg">发送信息</span>
   </div>
@@ -19,9 +20,8 @@ export default {
       // 有原生支持时，在页面加载后开启新的 Service Worker 线程，从而优化首屏加载速度
         window.addEventListener('load', function() {
         // register 方法里第一个参数为 Service Worker 要加载的文件；第二个参数 scope 可选，用来指定 Service Worker 控制的内容的子目录
-            navigator.serviceWorker.register('./ServiceWorker.js').then(function(registration) {
-            // Service Worker 注册成功
-            console.log('ServiceWorker 注册成功 ', registration.scope);
+            navigator.serviceWorker.register('./ServiceWorker.js',{scope: '/'}).then(function(registration) {
+                console.log('ServiceWorker 注册成功 ', registration.scope);
 
                 //接收信息
                 navigator.serviceWorker.addEventListener('message', function (event) {
@@ -29,8 +29,7 @@ export default {
                 })
 
             }).catch(function(err) {
-            // Service Worker 注册失败
-            console.log('ServiceWorker registration failed: ', err);
+                console.log('Service Worker 注册失败 failed: ', err);
             });
         });
         }
--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -2,13 +2,28 @@ var CACHE_NAME = '这是一个缓存';
 var urlsToCache = [
     '/admin.js'
 ];
-console.log(self)
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-    })
-  );
+	console.log('被安装之后触发事件')
+	  
+
+    event.waitUntil( // event.waitUtil 用于在安装成功之前执行一些预装逻辑
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
+
+
+self.addEventListener('waiting',event=>{
+	console.log('进入了等待状态')
+})
+
+
+self.addEventListener('activate',event=>{
+	console.log('进入了激活状态')
+})
+
+self.addEventListener('fetch', event => {
+	console.log('浏览器发起请求了')
+})
